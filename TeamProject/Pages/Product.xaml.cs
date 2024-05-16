33df671b5c8f02bf6d9c9cc6049b075e5f1f6001
jonyLon <masterlordgoker@gmail.com>
2024-05-16 22:05:05 +0300

    add layout and buttons

--- TeamProject/Pages/Product.xaml.cs
+++ TeamProject/Pages/Product.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TeamProject
+namespace TeamProject.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Product.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Product : Page
     {
-        public MainWindow()
+        public Product()
         {
-            mainFrame.Navigate(new Product());
+            InitializeComponent();
         }
-
-
     }
-    // 2121
 }
